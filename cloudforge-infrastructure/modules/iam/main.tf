aa423bb5571b9eed88835a0c47d3f717a0c82db7
Asim Abbasi <asimabbasi1029@gmail.com>
2026-08-27 15:57:12 +0500

    fix(ci): add case-insensitive OIDC matching, official thumbprints, and job permissions

--- cloudforge-infrastructure/modules/iam/main.tf
+++ cloudforge-infrastructure/modules/iam/main.tf
@@ -297,7 +297,11 @@ data "tls_certificate" "github_actions" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+    data.tls_certificate.github_actions.certificates[0].sha1_fingerprint
+  ]
 
   tags = var.tags
 }
@@ -320,7 +324,10 @@ resource "aws_iam_role" "github_actions_ecr" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:${var.github_repo}:*",
+              "repo:${lower(var.github_repo)}:*"
+            ]
           }
         }
       }
